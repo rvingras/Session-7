04ec4622c91b065ccdf2c3ecd21738bb1c2535e5
rvingras <vinr1_12@uni.worc.ac.uk>
2016-11-17 15:58:53 +0000

    -

--- touchGestureCode/www/js/index.js
+++ touchGestureCode/www/js/index.js
@@ -1,22 +1,25 @@
 <!-- For more info on jQuery Mobile,  touch gestures and other useful events see : http://api.jquerymobile.com/category/events/ -->
 
+
+
 $(document).on("pagecreate","#pageone",function(){
   
-  	$('#tapholdtext').on("taphold",function(){
-    	$(this).hide();
- 	});                       
-
-	$('#taptext').on("tap",function(){
-    	function random() {
-	       return !Math.round(Math.random());
-            window.localStorage.getItem(“random”);
-}
-
- 	}); 
-
-	$('#swipetext').on("swipeleft",function(){
-    	$(this).css('color', 'green');
-  	});   
-
+  	$('#taptext').on("tap",function(){
+    	
+          var randombutton = random();
+            alert(randombutton);
+    }}); 
+    
+    //$('#tapholdtext').on("taphold",function(){
+    	//$(this).hide();
+ 	//});
+    
+    //$('#swipetext').on("swipeleft",function(){
+    	//$(this).css('color', 'green');
+  	//});   
 
 });
+
+function random() {
+	       return !Math.round(Math.random());
+}
